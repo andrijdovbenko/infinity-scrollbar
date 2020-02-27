834f62df4ec71d651a2a938368f787ef349aac02
Andrii Dovbenko <andrijdovbenko@gmail.com>
2020-02-27 16:09:28 +0200

    saving data to Map, displaying dummy data while loading data from server

--- src/Vlist.js
+++ src/Vlist.js
@@ -12,7 +12,9 @@ class Vlist extends React.Component{
             end:this.numVisibleItems,
             data: []     
         }
-        this.loadDataFromServer(this.state.start, this.state.end);
+        this.map = new Map();
+        this.timerIds = [];
+        this.loadData(this.state.start, this.state.end);
         this.containerStyle={height:this.dataLength * this.props.itemheight}
         
         this.scollPos=this.scollPos.bind(this)
@@ -29,20 +31,50 @@ class Vlist extends React.Component{
         }
     }
 
-    loadDataFromServer(start, end) {
-        setTimeout(() => {
-            const data = []
-            start = start - this.renderedItemsRange < 0 ? 0 : start - this.renderedItemsRange;
-            end = end + this.renderedItemsRange > this.dataLength ? this.dataLength - 1 : end + this.renderedItemsRange;
-            for (let i = start; i <= end; i++){
-                data.push({name: `Row ${i}`, id: i});
-            }
+    loadData(start, end) {
+        start = start - this.renderedItemsRange < 0 ? 0 : start - this.renderedItemsRange;
+        end = end + this.renderedItemsRange > this.dataLength ? this.dataLength - 1 : end + this.renderedItemsRange;
+        this.getDataFromMap(start, end);
+        if (!this.isRequestRequered) {
+            this.abortRequests();
+        } else {
+            this.getDataFromServer(start, end);
+        }
+        
+    }
 
-            this.data = data
+    getDataFromServer(start, end){
+        this.abortRequests();
+        const timerId = setTimeout(() => {
+            const data = []
+            for (let i = start; i <= end; i++){
+                const item = {name: `Row ${i}`, id: i}
+                data.push(item);
+                this.map.set(i, item);
+            }
+            console.log('data  from server', data);
             this.setState({...this.state, data});
         }, 500);
+        this.timerIds.push(timerId);
     }
-    
+
+    abortRequests() {
+        this.timerIds.map((timerId => clearInterval(timerId)));
+    }
+
+    getDataFromMap(start, end) {
+        let isRequestRequered = false
+        const data = [];
+        for (let i = start; i <= end; i++) {
+            if (!this.map.has(i)) isRequestRequered = true;
+            const item = this.map.get(i) || {name: `Row`, id: i}
+            data.push(item);
+        }
+
+        this.setState({...this.state, data});
+        this.isRequestRequered = isRequestRequered;
+    }
+
     renderRows(){
         let result=[];
         for (let i=this.state.start - this.renderedItemsRange;i<this.state.end + this.renderedItemsRange;i++){
@@ -56,7 +88,7 @@ class Vlist extends React.Component{
 
     componentDidUpdate(prevProps, prevState) {
         if (prevState.start !== this.state.start || prevState.end !== this.state.end) {
-            this.loadDataFromServer(this.state.start, this.state.end);
+            this.loadData(this.state.start, this.state.end);
         }
     }
     
